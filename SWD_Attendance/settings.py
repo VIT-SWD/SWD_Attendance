cf3675ff6f330d1a84803f4536dded61a4556fd0
Pranav <pranav.doshi221@vit.edu>
2024-10-15 23:21:25 +0530

    Updated settings.py

--- SWD_Attendance/settings.py
+++ SWD_Attendance/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f7!_bb*67rwd-)&7=zbz%jxhb*ftci6adg$)lue4g7ms0ebf8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
